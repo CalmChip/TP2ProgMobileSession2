1332091689578f9fece19b724984dedfb9d3de26
Jean-Bernard Le Blanc <108352489+CalmChip@users.noreply.github.com>
2022-11-21 15:25:20 -0500

    Removed useless junk

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -8,7 +8,6 @@ import MainListe from "../screens/MainListe";
 import Conversation from "../screens/Conversation";
 import { MaterialIcons } from "@expo/vector-icons";
 import { clearData } from "../services/userServices";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Drawer = createDrawerNavigator();
 
